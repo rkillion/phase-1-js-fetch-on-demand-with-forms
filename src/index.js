7a59c1ca7c5944c656688b82ee8a82a3080d92a9
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-07-25 09:22:20 -0400

    commit tree ab781944f62d8db8add3a7f7facc1b96e054acd0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+function getIDSelection(event){
+    event.preventDefault();
+    const idInput = document.getElementById("searchByID").value;
+    fetch(`http://localhost:3000/movies/${idInput}`).then(resp => resp.status===404 ? alert("Please enter a valid ID") : resp.json()).then(json => renderMovie(json));
+}
+
+function renderMovie(obj) {
+    const titleHeader = document.querySelector("#movieDetails > h4");
+    const summaryPar = document.querySelector("#movieDetails > p");
+    const idInputField = document.getElementById("searchByID");
+    idInputField.value = "";
+    titleHeader.textContent = obj.title;
+    summaryPar.textContent = obj.summary;
+}
+
 const init = () => {
-  
+    const inputForm = document.querySelector("body > section:nth-child(3) > form");
+    inputForm.addEventListener("submit",getIDSelection);
 }
 
 document.addEventListener('DOMContentLoaded', init);
